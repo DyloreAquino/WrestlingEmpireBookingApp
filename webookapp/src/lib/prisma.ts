f37aa4f22217cca7898878a68b1483bdcf14e642
DyloreAquino <lutherjerold@gmail.com>
2026-02-20 09:59:15 +0800

    Revert "Fixed Issues for initialization of database"
    This reverts commit d6b8988ea025fffed0d2960538a5b3ede8f61344.

--- webookapp/src/lib/prisma.ts
+++
@@ -1,9 +0,0 @@
-// src/lib/prisma.ts
-import { PrismaClient } from "../generated/client";
-
-declare global {
-  var prisma: PrismaClient | undefined;
-}
-
-// Minimal options object for local SQLite
-export const prisma = new PrismaClient();
