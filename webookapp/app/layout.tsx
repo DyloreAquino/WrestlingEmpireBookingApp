23aae8402aefa5290f24fb4d90a6964efb7160df
DyloreAquino <lutherjerold@gmail.com>
2026-03-11 09:24:58 +0800

    Removed expect error comment from app layout

--- webookapp/app/layout.tsx
+++ webookapp/app/layout.tsx
@@ -2,7 +2,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Link from "next/link";
-// @ts-expect-error - CSS module types not available
 import "./globals.css";
 
 const geistSans = Geist({
